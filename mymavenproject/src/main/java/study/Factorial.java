49c4ee05fbaad75fad3ffe5c0d9451aea667cb11
HP <HP@192.168.1.41>
2023-11-21 08:24:10 +0530

    do

---
+++ mymavenproject/src/main/java/study/Factorial.java
@@ -0,0 +1,13 @@
+package study;
+
+public class Factorial {
+
+	public void fact() {
+		int f=1;
+		for(int i=1;i>4;i++) {
+			f=f*i;
+		}
+ System.out.println("The factorial of 4 is = "+f);
+	}
+
+}

6c00e7671ff5188a186e0c7d4497f194ca2b9e0a
HP <HP@192.168.1.41>
2023-11-21 08:35:21 +0530

    pusg

--- mymavenproject/src/main/java/study/C2.java
+++ mymavenproject/src/main/java/study/C2.java
@@ -2,12 +2,10 @@ package study;
 
 public class C2 extends Factorial {
 	
-	public void show() {
-		System.out.println("Hello from child");
-	}
+	
 public static void main(String args[]) {
 	C2 obj=new C2();
-	obj.show();
+	
 	obj.fact();
 }
 }
--- mymavenproject/src/main/java/study/Factorial.java
+++ mymavenproject/src/main/java/study/Factorial.java
@@ -3,6 +3,7 @@ package study;
 public class Factorial {
 
 	public void fact() {
+		int a=20;
 		int f=1;
 		for(int i=1;i>4;i++) {
 			f=f*i;
